90cec6f14d7b568da51d27fd48fbc0410dff724c
Shayne <Shayne6992@gmail.com>
2018-02-05 22:06:44 -0600

    Initial Commit

---
+++ Homework 2 New/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework 2 New
+//
+//  Created by Shayne Gill on 2/5/18.
+//  Copyright © 2018 Shayne Gill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
